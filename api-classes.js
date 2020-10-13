faf9541c6a5ba23983d930b00aac243e3fe29868
Danisa Ortanez <dortanez@yahoo.com>
2020-10-12 22:10:20 -0700

    hack snooze

--- api-classes.js
+++ api-classes.js
@@ -26,7 +26,7 @@ class StoryList {
   static async getStories() {
     // query the /stories endpoint (no auth required)
     const response = await axios.get(`${BASE_URL}/stories`);
-console.log(response)
+
     // turn the plain old story objects from the API into instances of the Story class
     const stories = response.data.stories.map(story => new Story(story));
 
@@ -47,6 +47,7 @@ console.log(response)
     // TODO - Implement this functions!
     // this function should return the newly created story so it can be used in
     // the script.js file where it will be appended to the DOM
+    const response = await axios.post(`${BASE_URL}/stories`,{user, newStory});
   }
 }
 
